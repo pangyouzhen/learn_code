4913d944e4dfadd2ee3b944c082c2094a106d76c
pangyouzhen <pangyouzhen@live.com>
2021-03-24 18:19:53 +0800

    flatten_json

--- utils/utils_func.py
+++ utils/utils_func.py
@@ -1,52 +1,3 @@
-import json
-import time
-import numpy as np
-
-
-def not_a_generator():
-    result = []
-    for i in range(2000):
-        result.append(i ** 2)
-    return result
-
-
-def is_a_generator():
-    for i in range(2000):
-        yield i ** 2
-
-
-def is_a_generator_ls():
-    return (i ** 2 for i in range(2000))
-
-
-n = time.time()
-print(not_a_generator())
-n1 = time.time()
-print(is_a_generator_ls())
-print([i for i in is_a_generator_ls()])
-print(n1 - n)
-print([i for i in is_a_generator()])
-print(time.time() - n1)
-
-
-def parser_js(x: str, keys_: str):
-    js = json.loads(x)
-    k = keys_.split(".")
-    try:
-        for i in k:
-            if i == "$":
-                # 1. 仅有一个key 2. key是不确定的
-                k = "".join(list(js.keys()))
-                js = js[k]
-                continue
-            js = js[i]
-    except KeyError:
-        return np.NAN
-    except AttributeError:
-        return np.NAN
-    return js
-
-
 def flat(x):
     for key, value in x.items():
         if isinstance(value, dict):
@@ -57,16 +8,43 @@ def flat(x):
             yield (key, value)
 
 
+#  用于将json 扁平化, 获取相应的key之后进行对比
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+
 if __name__ == '__main__':
     nest_dict = {
         'a': 1,
-        'b': {
+        'b': [{
             'c': 2,
             'd': 3,
             'e': {'f': 4}
         },
+            {
+                'c': 2,
+                'd': 3,
+                'e': {'f': 4}
+            }
+        ],
         'g': {'h': 5},
         'i': 6,
         'j': {'k': {'l': {'m': 8}}}
     }
     print({k: v for k, v in flat(nest_dict)})
+    print(flatten_json(nest_dict))
