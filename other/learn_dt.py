140ef2a32295e64202ae266047068ffe4e647ecc
pangyouzhen <pangyouzhen@live.com>
2021-06-30 17:39:41 +0800

    fixed

--- other/learn_dt.py
+++ other/learn_dt.py
@@ -1,19 +1,10 @@
-import datetime
-import time
+import arrow
 
-now = datetime.datetime.now()
-print('\n当前时间datetime：\t%s，类型：%s' % (now, type(now)))
-# 当前时间str
-now_str = str(now)
-print('当前时间str：\t\t%s，类型：%s' % (now_str, type(now_str)))
-# 当前时间时间戳
-now_mktime = time.time()
-print('当前时间时间戳：\t%s，类型：%s' % (now_mktime, type(now_mktime)))
-
-# 今天、昨天、明天
-today = datetime.date.today()
-yesterday = today - datetime.timedelta(days=1)
-tomorrow = today + datetime.timedelta(days=1)
-print('\n今天:%s,昨天:%s,明天：%s' % (today, yesterday, tomorrow))
-
-# 时间的转换 见 img/dt.jpg
+print(arrow.now())
+# 转化成字符串
+print(arrow.now().format())
+print(arrow.now().format("YYYY-MM-DD HH:mm:ss"))
+# 昨天
+print(arrow.now().shift(days=-1).format())
+# 转化为时间戳
+print(arrow.now().timestamp())
