4a103e05e62ad8fabb38f657a346df4e54253503
pangyouzhen <pangyouzhen@live.com>
2021-07-21 11:27:59 +0800

    async_df

--- utils/df_async_post.py
+++ utils/df_async_post.py
@@ -13,91 +13,103 @@ import uuid
 # logger.add("./a.log")
 
 
-class DfAsyncPost:
+class AsyncDf:
     """
-    针对dataframe的异步post请求封装
+    针对dataframe的异步请求封装
     """
 
-    def __init__(self, url: str, payload: str, df_response: str, sema: int = 5):
+    def __init__(self, df: pd.DataFrame, url: str, data: str, df_response: str, df_request_name: List[str],
+                 sema: int = 30, headers=None, method="post", **kwargs):
         """
         url:  url
-        payload : post请求的内容
         df_request_name: 请求列的列名称
         df_response： 保存在df的响应列的列名称
-        :rtype:
         """
         self.url = url
-        if isinstance(payload, str):
-            self.payload = payload
+        if isinstance(data, str):
+            self.data = data
         else:
-            assert "输入payload不是str的实例"
+            assert "输入data不是str的实例"
         self.df_response = df_response
-        self.headers = {"Content-Type": "application/json"}
-        self.headers_json = True
+        if headers is None:
+            self.headers = {"Content-Type": "application/json"}
+        else:
+            self.headers = headers
         self.sema = asyncio.BoundedSemaphore(sema)
+        self.df = df
+        self.df_request_name = df_request_name
+        self.method = method
 
     @logger.catch()
-    async def process_url(self, df: pd.DataFrame, ind_query: Tuple, df_request_name: List[str]) -> None:
+    async def process_url(self, ind_query: Tuple) -> None:
         """
         :type ind_query: 元组，第一个为index，第二个为需要格式化的内容，
         只传入单个内容：（0,"今天天气很好啊"）
         :rtype: object
         """
         ind, rows = ind_query
-        content = tuple(rows[df_request_name].tolist())
-        payload = self.payload % content
+        content = tuple(rows[self.df_request_name].tolist())
+        data = self.data % content
         with (await self.sema):
             async with aiohttp.ClientSession() as session:
-                resp = await session.post(self.url, json=json.loads(payload), headers=self.headers)
+                resp = await session.post(self.url, json=json.loads(data), headers=self.headers)
                 await asyncio.sleep(0.5)
-                df.loc[ind, self.df_response] = await resp.text()
+                self.df.loc[ind, self.df_response] = await resp.text()
 
-    # json.loads(payload)  就是将 str转化为字典，post 的参数中json 需要的是一个字典
-    # postman 中转化出来的是 str 需要改为data = payload.encode("utf-8")
+    # json.loads(data)  就是将 str转化为字典，post 的参数中json 需要的是一个字典
+    # postman 中转化出来的是 str 需要改为data = data.encode("utf-8")
     # a = json.loads("...
     # a.pop("preProcess")
     # a["data"] = a["data"].strip("\"")
 
-    async def gather_data(self, df: pd.DataFrame, df_request_name: List[str]) -> None:
+    async def gather_data(self):
         await asyncio.gather(
-            *[self.process_url(df, (ind, row), df_request_name) for ind, row in df.iterrows()])
+            *[self.process_url((ind, row)) for ind, row in self.df.iterrows()])
 
-    def run(self, df: pd.DataFrame, df_request_name: List[str]) -> pd.DataFrame:
+    def run(self) -> pd.DataFrame:
         start_time = datetime.datetime.now()
         print(f"start_time is {start_time}")
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.gather_data(df, df_request_name))
+        loop.run_until_complete(self.gather_data())
         loop.close()
         end_time = datetime.datetime.now()
         print(f"end_time is {end_time}")
         last_time = end_time - start_time
         print(f"last_time is {last_time}")
-        return df
+        return self.df
+
+    def __call__(self, *args, **kwargs):
+        return self.run()
 
     @classmethod
-    def from_postman_curl(cls, inputs, df_response):
+    def from_postman_curl(cls, df, curl_cmd, df_response, df_request_name, sema=30):
         """
         inputs: POSTMAN - curl
         """
-        try:
-            import uncurl
-        except:
-            assert "can't find uncurl"
-        context = uncurl.parse_context(inputs)
-
-        url = context.url
+        import uncurl
+        curl_cmd = curl_cmd.replace(" -L", "")
+        curl_cmd = curl_cmd.replace("--data-raw", "-d")
+        context = uncurl.parse_context(curl_cmd)
+        context_kwargs = context._asdict()
+        return AsyncDf(df=df, df_response=df_response, df_request_name=df_request_name, sema=sema, **context_kwargs)
 
 
 if __name__ == '__main__':
-    rand_num = np.random.randint(500, size=(100, 3))
+    rand_num = np.random.randint(500, size=(10000, 3))
     df = pd.DataFrame(rand_num)
     df.columns = ["user1", "user2", "user3"]
-    payload = """{\"username\": \"%s\",\"age\": \"%s\"}"""
+    data = """{\"username\": \"%s\",\"age\": \"%s\"}"""
     df_request_name = ["user1", "user2"]
-    df_async = DfAsyncPost(url="http://81.71.140.148:8082/reader", payload=payload, df_response="reponse", sema=100)
-    df_async.run(df, df_request_name)
+    # async_df = AsyncDf(df=df, url="http://127.0.0.1:8082/reader", data=data, df_response="reponse",
+    #                    df_request_name=df_request_name, sema=100)
+    # df = async_df()
+    # print(df[:5])
+    curl_cmd = """
+    curl -L -X POST 'http://127.0.0.1:8082/reader' \
+    -H 'Content-Type: application/json' \
+    --data-raw '{"username":"%s","age":"%s"}'
+    """
+    #
+    async_df = AsyncDf.from_postman_curl(df, curl_cmd, "response", df_request_name=["user1", "user2"], sema=100)
+    df = async_df()
     print(df[:5])
-    # curl_cmd = """curl --location --request POST 'http://127.0.0.1:8082/reader' \
-    #             --header 'Content-Type: application/json'
-    #             --data-raw '{"username":"pangyouzhen"}'"""
-    # df_async = DfAsyncPost.from_postman_curl(curl_cmd, "response")
