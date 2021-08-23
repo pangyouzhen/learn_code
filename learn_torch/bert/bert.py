7edfcd3c1bff0dfb7d1731325ae922c385cfb17a
pangyouzhen <pangyouzhen@live.com>
2021-08-23 16:57:55 +0800

    fixed

--- learn_torch/bert/bert.py
+++ learn_torch/bert/bert.py
@@ -1,22 +1,35 @@
+from typing import List
+
 import torch
+from torch import Tensor
+from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
 from transformers.models.bert import BertModel, BertTokenizer
 
 model_name = '/data/project/learn_code/data/chinese-bert-wwm-ext/'
 # 读取模型对应的tokenizer
-tokenizer = BertTokenizer.from_pretrained(model_name)
+tokenizer: BertTokenizer = BertTokenizer.from_pretrained(model_name)
 # 载入模型
-model = BertModel.from_pretrained(model_name)
+model: BertModel = BertModel.from_pretrained(model_name)
 # 输入文本
 # input_text = "Here is some text to encode"
 input_text = "今天天气很好啊,你好吗"
+# todo input_text2 怎样一起输入
+input_text2 = "文本"
 # 通过tokenizer把文本变成 token_id
-input_ids = tokenizer.encode(input_text, add_special_tokens=True)
+input_ids: List[int] = tokenizer.encode(input_text, add_special_tokens=True)
 print(len(input_ids))
-# input_ids: [101, 2182, 2003, 2070, 3793, 2000, 4372, 16044, 102]
-input_ids = torch.tensor([input_ids])
+print(input_ids)
+# 101 代表cls 符号，102代表 sep
+# [101, 791, 1921, 1921, 3698, 2523, 1962, 1557, 117, 872, 1962, 1408, 102]
+input_ids: Tensor = torch.tensor([input_ids])
 # 获得BERT模型最后一个隐层结果
 print(input_ids.shape)
 with torch.no_grad():
-    last_hidden_states = model(input_ids)[0]
-    print(last_hidden_states)
-    print(last_hidden_states.shape)
+    # bert 的输入为 batch_size,seq_length
+    a: BaseModelOutputWithPoolingAndCrossAttentions = model(input_ids)
+    last_hidden_state = a.last_hidden_state
+    # batch_size,seq_length,embedding
+    print(torch.equal(last_hidden_state[:, 0, :], a.pooler_output))
+    # last_hidden_state 的输出为 batch_size,seq_length, embedding
+    print(last_hidden_state)
+    print(last_hidden_state.shape)
