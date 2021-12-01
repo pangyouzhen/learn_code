806c345b1a8b96ff3cbe85e0798977354fa6c679
pang <pangyouzhen@live.com>
2021-12-01 21:19:24 +0800

    fixed

--- utils/batch_img2pdf.py
+++ utils/batch_img2pdf.py
@@ -2,15 +2,21 @@ from pathlib import Path
 
 from PIL import Image
 
-img1 = Image.open("./temp/0.jpg")
-im_list = []
+
+def concat_img(path):
+    img1 = Image.open(path / "0.jpg")
+    im_list = []
+    imgs = sorted(path.glob("*jpg"), key=lambda x: int(x.stem))
+    for i in imgs:
+        if i.stem == "0":
+            continue
+        im_list.append(Image.open(str(i)))
+
+    pdf1_filename = path / "直线检测.pdf"
+
+    img1.save(pdf1_filename, "PDF", resolution=100.0, save_all=True, append_images=im_list)
+
+
 path = Path("./temp")
-imgs = sorted(path.glob("*jpg"), key=lambda x: int(x.stem))
-for i in imgs:
-    if i.stem == "0":
-        continue
-    im_list.append(Image.open(str(i)))
-
-pdf1_filename = "./face.pdf"
-
-img1.save(pdf1_filename, "PDF", resolution=100.0, save_all=True, append_images=im_list)
+for i in path.iterdir():
+    concat_img(i)
--- utils/screen_fetch2.py
+++ utils/screen_fetch2.py
@@ -6,30 +6,69 @@ import keyboard
 import pyautogui
 from loguru import logger
 
-pyautogui.hotkey("winleft", "2")
+# 0: 不做任何操作，适合单个文件
+# 1: 竖屏的pdf，方法先转为横屏，然后全屏，合并成pdf后再转回来
+# 2. 横屏ppt
+# 3. txt类型的，用作翻页的前置条件+切换上
+# 4. 横屏pdf
+keyboard.press_and_release('left windows + 2')
 logger.info("open success")
 time.sleep(5)
 
-all_ = [73, 130, 28, 1832]
-_ = [ceil((i / 49)) for i in all_]
-print(_)
+
+def ctrl_tab():
+    keyboard.press("ctrl")
+    time.sleep(2)
+    keyboard.press_and_release("tab")
+    time.sleep(2)
+    keyboard.release("ctrl")
+
+
+# _ = [ceil((i / 49)) for i in all_]
+# print(_)
+file_type = 0
+all_ = [3]
 for i in range(len(all_)):
-    path = Path(f"./cv/{i}")
+    path = Path(f"./temp/{i}")
     path.mkdir(exist_ok=True)
-    if i != 0:
-        keyboard.press("ctrl")
-        time.sleep(2)
-        keyboard.press_and_release("tab")
-        time.sleep(2)
-        keyboard.release("ctrl")
-        time.sleep(2)
-    keyboard.press_and_release("home")
-    logger.info("home")
-    for j in range(_[i]):
+    if file_type == 3:
+        # 49 是一个文本编辑器 **全屏** 所展示的行数
+        all_ = [ceil((i / 49)) for i in all_]
+    for j in range(all_[i]):
         img = pyautogui.screenshot()
-        img.save(f'./cv/{i}/{j}.jpg')
+        img.save(f'./temp/{i}/{j}.jpg')
         logger.info(f"save {j} success")
         keyboard.press_and_release("pagedown")
         time.sleep(3)
     time.sleep(5)
-    logger.info("进入下一个")
+    # 竖屏的pdf
+    if file_type == 1:
+        logger.info("竖屏pdf--进入下一个pdf")
+        # 退出pdf全屏
+        keyboard.press_and_release("esc")
+        time.sleep(4)
+        # 切换下一个pdf
+        ctrl_tab()
+        time.sleep(4)
+        # 顺时针旋转
+        keyboard.press_and_release("ctrl+shift+=")
+        time.sleep(4)
+        # 进入全屏
+        keyboard.press_and_release("ctrl+l")
+        time.sleep(6)
+    elif file_type == 2:
+        logger.info("横屏ppt-进入下一个")
+        keyboard.press_and_release("esc")
+        time.sleep(4)
+        # 切换下一个pdf
+        ctrl_tab()
+        time.sleep(4)
+        keyboard.press_and_release("f5")
+        time.sleep(6)
+    elif file_type == 3:
+        logger.info("文本类型-进入切换")
+        time.sleep(3)
+        ctrl_tab()
+        time.sleep(4)
+    elif file_type == 4:
+        pass
