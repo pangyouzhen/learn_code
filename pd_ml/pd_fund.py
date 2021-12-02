67d6675cb85215f3eaf7d0d05a371432b698ea56
pang <pangyouzhen@live.com>
2021-12-02 18:22:13 +0800

    fixed

--- pd_ml/pd_fund.py
+++ pd_ml/pd_fund.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pathlib import Path
+from typing import List, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,19 +8,62 @@ import pandas as pd
 from loguru import logger
 from pandas.core.groupby import DataFrameGroupBy
 
+
 # 数据分析还是建议使用jupyter
 # 普通的数据分析和具体上系统是不一样的
 # 数据的一般流程
 
 # logger 的一些设置
 
-# 使用前最好assert pandas的版本，防止版本不同导致函数接口不同
-# 对于上一个新的系统而言，对行数和列数，列名称检查都是必要的
-try:
-    assert float(pd.__version__[:3]) >= 1.0
-except Exception as e:
-    logger.error(f"pd的版本为{pd.__version__},不符合版本")
-    raise e
+
+def check_pd_version():
+    # 检查pd版本, 防止因为版本不同导致接口不同
+    # 适合整体服务启动初始化时检查
+
+    try:
+        assert float(pd.__version__[:3]) >= 1.2
+    except Exception as e:
+        logger.error(f"pd的版本为{pd.__version__},不符合版本")
+        raise e
+
+
+class PreProcess:
+    # 进行dataframe的预处理，包括列名称的检查，df的类型转换
+    @staticmethod
+    def check_df(df: pd.DataFrame,
+                 check_columns: bool = False,
+                 column_name: Optional[List[str]] = None,
+                 ) -> None:
+        if check_columns:
+            if set(df.columns) != set(column_name):
+                logger.error("assert的名称和输入的不同，请检查")
+        if df.shape[0] >= 10 ** 7:
+            logger.error(f"df的维度是{df.shape}数据的行数过大，请分块输入")
+        if df.shape[1] == 0:
+            logger.error("输入的空列，请检查")
+        if df.index.duplicated is True:
+            logger.error("输入的index有重复值")
+        logger.info("输入的df的维度的时{df.shape}")
+
+    @staticmethod
+    def convert_dtypes_na(df: pd.DataFrame) -> pd.DataFrame:
+        df = df.convert_dtypes()
+        contain_na_colums: List[str] = df.columns[df.isnull().any()].tolist()
+        for i in df.select_dtypes(include=np.number).columns:
+            if i not in contain_na_colums:
+                # Float64Dtype() 在1.2 版本中
+                if isinstance(df[i].dtypes, pd.Float64Dtype()):
+                    df[i] = df[i].astype(np.float32)
+                else:
+                    df[i] = df[i].astype(np.int32)
+        return df
+
+    def __call__(self, df: pd.DataFrame) -> pd.DataFrame:
+        self.check_df(df)
+        self.df = self.convert_dtypes_na(df)
+        return self.df
+
+
 path = Path("../learn_torch/.data/train.csv")
 df = pd.read_csv(path, sep="\t", names=["sent0", "sent1", "label"])
 df = df.convert_dtypes()
@@ -36,10 +80,11 @@ left = pd.DataFrame(
         "a": [1, 2, 3],
         "dt": ["2021/6/11", "2021/07/12", "2021/03/02"],
         "b": [4, 1, np.NAN],
-        "c": ["a", "b", "c"]
+        "c": ["a", "b", "c"],
+        "d": [3.0, 4.0, 7.1],
     }
 )
-ind: pd.DatetimeIndex = pd.DatetimeIndex(['2021-09-09', '2021-09-11', '2021-09-10'])
+ind: pd.DatetimeIndex = pd.DatetimeIndex(["2021-09-09", "2021-09-11", "2021-09-10"])
 left.index = ind
 
 right: pd.DataFrame = pd.DataFrame(
@@ -48,7 +93,7 @@ right: pd.DataFrame = pd.DataFrame(
         "dt": ["2021/6/11", "2021/07/12", "2021/03/02", "2021/09/11"],
         "b": [4, 1, np.NAN, np.NAN],
         "c": ["a", "b", "c", "e"],
-        "y": [0, 1, 1, 0]
+        "y": [0, 1, 1, 0],
     }
 )
 left = left.convert_dtypes()
@@ -63,16 +108,18 @@ df2: pd.DataFrame = left.loc[left["dt"] < dt, :]
 df3: pd.DataFrame = left.loc[left["dt"] < dt, ["a", "b"]]
 df4: pd.DataFrame = left.loc[(left["dt"] < dt) & (left["b"] < 4), ["a", "b"]]
 df5: pd.Series = right.loc[right["dt"] < dt, "y"]
-df3: pd.DataFrame = df3[~df3.index.duplicated(keep='first')]
+df3: pd.DataFrame = df3[~df3.index.duplicated(keep="first")]
 # pandas 自动推断并转换类型
-for i in left.select_dtypes(exclude=np.number).columns:
+for i in df.select_dtypes(include=np.number).columns:
     print(i)
+for i in df.select_dtypes(include=np.int64).columns:
+    df[i] = df[i].astype(np.int32)
 print("-----------------------------------------")
 # 这些默认生成的都是series
 print(left.dtypes)
 print(type(left.dtypes))
 # accessors
-left['c'] = left["c"].astype("category").cat.codes
+left["c"] = left["c"].astype("category").cat.codes
 print(left)
 # print(df1 == df2)
 # 选择列,单列可以使用df["b"],多列使用df[["b"]]
@@ -95,14 +142,11 @@ ku = right.kurt().to_frame("峰度")
 df_type = right.dtypes.to_frame("数据类型")
 nuniq = right.nunique().to_frame("数据值的个数")
 # 堆积柱状图
-df = pd.DataFrame(
-    {"x": [1, 2, 2, 0, 2],
-     "y": [0, 1, 0, 1, 0]}
-)
+df = pd.DataFrame({"x": [1, 2, 2, 0, 2], "y": [0, 1, 0, 1, 0]})
 a: DataFrameGroupBy = df.groupby(["x", "y"])
 for i, v in a:
     pass
-df.groupby(["x", "y"]).size().unstack().plot(kind='bar', stacked=True)
+df.groupby(["x", "y"]).size().unstack().plot(kind="bar", stacked=True)
 plt.show()
 plt.close()
 print(left.kurt())
@@ -113,16 +157,19 @@ print(df)
 
 np.random.seed(100)
 
-df = pd.DataFrame({'grade': np.random.choice(list('ABCD'), size=(20)),
-                   'pass': np.random.choice([0, 1], size=(20))
-                   })
+df = pd.DataFrame(
+    {
+        "grade": np.random.choice(list("ABCD"), size=(20)),
+        "pass": np.random.choice([0, 1], size=(20)),
+    }
+)
 
-feature, target = 'grade', 'pass'
-df_woe_iv = (pd.crosstab(df[feature], df[target],
-                         normalize='columns')
-             .assign(woe=lambda dfx: np.log(dfx[1] / dfx[0]))
-             .assign(iv=lambda dfx: np.sum(dfx['woe'] *
-                                           (dfx[1] - dfx[0]))))
+feature, target = "grade", "pass"
+df_woe_iv = (
+    pd.crosstab(df[feature], df[target], normalize="columns")
+        .assign(woe=lambda dfx: np.log(dfx[1] / dfx[0]))
+        .assign(iv=lambda dfx: np.sum(dfx["woe"] * (dfx[1] - dfx[0])))
+)
 
 print(df_woe_iv)
 
@@ -141,7 +188,7 @@ df_all = pd.DataFrame(
         "stock_code": ["1", "1", "3"],
         "date": ["2021/6/11", "2021/07/12", "2021/6/11"],
         "value": [4, 1, np.NAN],
-        "other": ["a", "b", "c"]
+        "other": ["a", "b", "c"],
     }
 )
 df_all = df_all.convert_dtypes()
