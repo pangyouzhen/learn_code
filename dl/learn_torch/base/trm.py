2859ee3df569f79f71b663f42ff1354fab0ae65a
pang <pangyouzhen@live.com>
2022-03-27 17:31:01 +0800

    fixed

--- dl/learn_torch/base/trm.py
+++ dl/learn_torch/base/trm.py
@@ -1,74 +1,35 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 from torch.nn import TransformerEncoderLayer
 import torch.nn.functional as F
 from torch.nn.init import xavier_uniform_
 from torch.nn.modules.activation import MultiheadAttention
 
+def scaled_dot_product_attention(query:Tensor,key:Tensor,value:Tensor) -> Tensor:
+    temp = query.bmm(key.transpose(1,2))
+    scale = query.size(-1) ** 0.5
+    softmax = F.softmax(temp/scale,dim=-1)
+    return softmax.bmm(value)
 
-# TODO
-class TrmEncoder(nn.Module):
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+class AttentionHead(nn.Module):
+    def __init__(self,dim_in:int,dim_q:int,dim_k:int) -> None:
         super().__init__()
-        self.self_attn = MA(d_model, nhead)
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-        self.activation = F.relu
-
-    def forward(self, src):
-        src2 = self.self_attn(src)[0]
-        src = src + self.dropout1(src2)
-        src = self.norm1(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-        src = src + self.dropout2(src2)
-        src = self.norm2(src)
-        return src
-
-
-# 多头注意力机制
-class MA(nn.Module):
-    def __init__(self, embed_dim, nhead):
-        super().__init__()
-        self.qkv_para = xavier_uniform_(nn.Parameter(torch.empty(3 * embed_dim, embed_dim)))
-        # TODO
-        # 这里为什么全部置为0,才不会发生梯度消失的问题
-        self.bias = nn.Parameter(torch.zeros(3 * embed_dim))
-        self.nhead = nhead
-        self.head_dim = embed_dim // nhead
-        self.out_proj_weight = xavier_uniform_(nn.Parameter(torch.empty(embed_dim, embed_dim)))
-
-    def forward(self, src: torch.Tensor):
-        # self-attention
-        q, k, v = F.linear(src, self.qkv_para, self.bias).chunk(3, dim=-1)
-        bsz, seq_len, embed_dim = src.size()
-        src_len = k.size(1)
-        q = q.contiguous().view(seq_len, bsz * self.nhead, self.head_dim).transpose(0, 1)
-        k = k.contiguous().view(-1, bsz * self.nhead, self.head_dim).transpose(0, 1)
-        v = v.contiguous().view(-1, bsz * self.nhead, self.head_dim).transpose(0, 1)
-        attn_output_weights = q @ k.transpose(1, 2)
-        attn_output_weights = F.softmax(
-            attn_output_weights, dim=-1)
-        attn_output_weights = F.dropout(attn_output_weights, p=0.1)
-        attn_output = attn_output_weights @ v
-        attn_output = attn_output.transpose(0, 1).contiguous().view(seq_len, bsz, embed_dim)
-        attn_output = F.linear(attn_output, self.out_proj_weight)
-        # print(f"{attn_output.shape = }",f"{attn_output_weights.shape = }")
-        attn_output_weights = attn_output_weights.view(bsz, self.nhead, seq_len, src_len)
-        return attn_output, attn_output_weights
+        self.q = nn.Linear(dim_in,dim_q)
+        self.k = nn.Linear(dim_in,dim_k)
+        self.v = nn.Linear(dim_in,dim_k)
+        
+    def forward(self,query:Tensor,key:Tensor,value: Tensor) -> Tensor:
+        return scaled_dot_product_attention(self.q(query),self.k(key),self.v(value))
 
 
 if __name__ == '__main__':
     # trm_encoder = TrmEncoder(d_model=512, nhead=8)
-    ma = MA(512,8)
+    # ma = MA(512,8)
     bulitin_multiha = MultiheadAttention(512,8)
     src = torch.rand(10, 32, 512)
     print("raw_shape",src.shape)
-    ma_res = ma(src,)
+    # ma_res = ma(src,)
     print("not built-in----",ma_res[0].shape,ma_res[1].shape)
     multiha_res = bulitin_multiha(src,src,src)
     print("builtin shape+++++",multiha_res[0].shape,multiha_res[1].shape)
--- utils/init_shell/.bashrc
+++ utils/init_shell/.bashrc
@@ -33,33 +33,11 @@ lg() {
   fi
 }
 
-# manjaro bash 解压方法
-ex() {
-  if [ -f $1 ]; then
-    case $1 in
-    *.tar.bz2) tar xjf $1 ;;
-    *.tar.gz) tar xzf $1 ;;
-    *.bz2) bunzip2 $1 ;;
-    *.rar) unrar x $1 ;;
-    *.gz) gunzip $1 ;;
-    *.tar) tar xf $1 ;;
-    *.tbz2) tar xjf $1 ;;
-    *.tgz) tar xzf $1 ;;
-    *.zip) unzip $1 ;;
-    *.Z) uncompress $1 ;;
-    *.7z) 7z x $1 ;;
-    *) echo "'$1' cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
-}
-
  port_path(){ 
   lsof -i:$1 | awk '{print $2}' | grep -v PID | xargs pwdx
  }
 
-alias vv="virtualenv venv"
+alias vv="virtualenv venv && source ./venv/bin/activate"
 alias docker_remove_exit="docker ps -a | grep Exit | awk '{print \$1}' | xargs docker rm"
 alias docker_restart="sudo systemctl restart docker && sudo chmod 666 /var/run/docker.sock"
 alias docker_remove_none="docker images | grep none | awk '{print \$3}' | xargs docker rmi"
@@ -72,4 +50,3 @@ alias mkdir="mkdir -pv"
 alias rsync-stock="rsync -avz root@81.71.140.148:/data/project/stock ./ --exclude venv --exclude __pycache__ --exclude .git --exclude .idea --exclude log"
 alias s148="ssh root@81.71.140.148"
 alias python="python3"
-
